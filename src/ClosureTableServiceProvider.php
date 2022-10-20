725d6d610d6d604813469077dd5403ab72d1ec8e
KerTag <66644934@qq.com>
2022-10-20 11:52:02 +0800

    kertag

---
+++ src/ClosureTableServiceProvider.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Kertag\ClosureTable;
+
+use Illuminate\Support\ServiceProvider;
+
+class ClosureTableServiceProvider extends ServiceProvider
+{
+    /**
+     * 服务提供者加是否延迟加载.
+     */
+
+    protected $defer = true; // 延迟加载服务
+
+    /**
+     * Bootstrap the application services.
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__ . '/config/closure-table.php' => config_path('closure-table.php'),
+        ]);
+    }
+
+    /**
+     * Register the application services.
+     */
+    public function register()
+    {
+        $this->app->singleton('closure-table', function () {
+            return new ClosureTable;
+        });
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+
+    {
+        // 因为延迟加载 所以要定义 provides 函数 具体参考laravel 文档
+        return ['closure-table'];
+    }
+}
